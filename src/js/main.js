360a14bbc4a70d6bf6dbd7486bc763e60a8bedf1
Amulek Angulo <amulek.angulo@ansira.com>
2023-01-25 08:38:17 -0600

    w04-team-activity-changes

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
+
+const prod = new ProductData();
+
+const listp = new ProductList();
+
+console.log(listp)
+
+
+
